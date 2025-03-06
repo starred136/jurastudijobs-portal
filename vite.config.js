2662cbbfbe9a7b77c96fa2c0fb992a331a1cf2f4
Tangmoh Francky <franckycyrille47@gmail.com>
2025-03-06 14:59:40 +0100

    let-commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // ✅ Ensures that all routes are served from index.html
+  },
   build: {
     outDir: "dist", // ✅ Ensures correct output directory
     rollupOptions: {
