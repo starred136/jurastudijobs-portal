3b1496d871bce89ac751429a47f355dc735b3304
Tangmoh Francky <franckycyrille47@gmail.com>
2025-03-06 11:02:43 +0100

    let add jobdetailpage

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -63,14 +63,33 @@ export const jobList = [
     semesterImage: "/assets/images/semester2.png",
     tags: ["Vertragsklauseln", "Übersetzunge"],
     description: `
-      <h2>Job Description</h2>
-      <p>Join our legal team as a Werkstudent and gain hands-on experience in contract law.</p>
-      <h3>What You Will Do:</h3>
-      <ul>
-        <li>Translate legal documents</li>
-        <li>Review contract clauses</li>
-        <li>Support compliance processes</li>
-      </ul>
+      <h2>Vollständige Stellenbeschreibung</h2>
+                            <p>Für unseren Kunden GHC Gerling, Holz &amp; Co. Handels GmbH in Hanau suchen wir zum nächstmöglichen Zeitpunkt eine/n.</p>
+                            <h2>Das erwartet Dich</h2>
+                            <ul>
+                                <li>Prüfung und Analyse von Vertragsklauseln für den internationalen Export (insbesondere US Verwaltung Einkauf Rules (FAR)).</li>
+                                <li>Übersetzung von Klauseln aus dem Englischen ins Deutsche.</li>
+                                <li>Mitwirkung bei der Erstellung von Verträgen für Exportgeschäfte.</li>
+                                <li>Allgemeine administrative Tätigkeiten im Rechtsbereich.</li>
+                                <li>Unterstützung in rechtlichen Fragestellungen im Zusammenhang mit dem internationalen Handel.</li>
+                            </ul>
+                            <h2>Das erwarten wir</h2>
+                            <ul>
+                                <li>Eingeschriebener Student im Bereich Rechtswissenschaften.</li>
+                                <li>Gute Englischkenntnisse in Wort und Schrift.</li>
+                                <li>Interesse am internationalen Handelsrecht und Vertragsrecht.</li>
+                                <li>Sorgfältige und zuverlässige Arbeitsweise sowie Vertrauenswürdigkeit.</li>
+                                <li>Selbständige und strukturierte Arbeitsweise.</li>
+                            </ul>
+                            <h2>Das können wir Dir anbieten</h2>
+                            <ul>
+                                <li>Flexible Arbeitszeiten.</li>
+                                <li>Homeoffice.</li>
+                                <li>Flache Hierarchien und kurze Entscheidungswege.</li>
+                                <li>Spannende Einblicke in die rechtlichen Aspekte des Exports von Gasen und Chemikalien.</li>
+                                <li>Möglichkeit zur persönlichen und fachlichen Weiterentwicklung.</li>
+                            </ul>
+                            <p>Wir haben Ihr Interesse geweckt? Dann senden Sie uns Ihre kompletten Bewerbungsunterlagen mit Anschreiben, Gehaltsvorstellung und Verfügbarkeit per Mail in PDF an.</p>
     `,
   },
   {
@@ -84,14 +103,40 @@ export const jobList = [
     semesterImage: "/assets/images/semester3.png",
     tags: ["Unterstützung", "Datenschutz-Management-Systeme", "MS Office"],
     description: `
-      <h2>Job Description</h2>
-      <p>Join our legal team as a Werkstudent and gain hands-on experience in contract law.</p>
-      <h3>What You Will Do:</h3>
-      <ul>
-        <li>Translate legal documents</li>
-        <li>Review contract clauses</li>
-        <li>Support compliance processes</li>
-      </ul>
+      <h2>Vollständige Stellenbeschreibung</h2>
+        <p>
+            G+D macht das Leben von Milliarden von Menschen weltweit sicherer. Wir schaffen Vertrauen im digitalen Zeitalter,
+            mit integrierten Sicherheitstechnologien in drei Geschäftsbereichen: Digital Security, Financial Platforms und Currency Technology.
+        </p>
+        <h2>Ihre Aufgaben:</h2>
+        <ul>
+            <li>Unterstützung des internen Datenschutzteams bei der Überprüfung und Umsetzung der DSGVO-Bestimmungen.</li>
+            <li>Selbständige Übernahme von Aufgaben nach Einarbeitung.</li>
+            <li>Betreuung des globalen Datenschutz-Management-Systems.</li>
+            <li>Erstellung von Dokumenten und Checklisten gemäß den Anweisungen.</li>
+        </ul>
+        <h2>Ihr Profil:</h2>
+        <ul>
+            <li>Immatrikulierter Student der Fachrichtung Jura, Wirtschaftsrecht, BWL oder mit IT-Schwerpunkt.</li>
+            <li>Mindestens 12 Monate immatrikuliert.</li>
+            <li>Interesse an datenschutzrechtlichen Themen.</li>
+            <li>Gute Kenntnisse in MS-Office, insbesondere Word, Excel und PowerPoint.</li>
+        </ul>
+        <h2>Wir bieten:</h2>
+        <ul>
+            <li>Ein angenehmes Arbeitsumfeld in einem internationalen Unternehmen.</li>
+            <li>Flexible Arbeitszeiten und Möglichkeit zum mobilen Arbeiten.</li>
+            <li>Schulung durch unseren Datenschutzbeauftragten.</li>
+        </ul>
+        <h2>Kontakt</h2>
+        <p><b>Juliane Stang</b></p>
+        <a href="mailto:join-gd@gi-de.com">join-gd@gi-de.com</a>
+        <h2>Job Details</h2>
+        <p><b>Stellenbezeichnung:</b> Werkstudent Datenschutz (m/w/d)</p>
+        <p><b>Unternehmensbereich:</b> Giesecke+Devrient GmbH</p>
+        <p><b>Standort:</b> München, DE</p>
+        <p><b>Karrierestufe:</b> Werkstudenten</p>
+        <p><b>Vertragsart:</b> Teilzeit, Befristet</p>
     `,
   },
   {
@@ -105,14 +150,56 @@ export const jobList = [
     semesterImage: "/assets/images/semester2.png",
     tags: ["Vertragsdaten", "Klassifizierung", "Dokumentation", "Prüfung"],
     description: `
-      <h2>Job Description</h2>
-      <p>Join our legal team as a Werkstudent and gain hands-on experience in contract law.</p>
-      <h3>What You Will Do:</h3>
-      <ul>
-        <li>Translate legal documents</li>
-        <li>Review contract clauses</li>
-        <li>Support compliance processes</li>
-      </ul>
+      <h2>Vollständige Stellenbeschreibung</h2>
+    <p>
+      Zum nächstmöglichen Zeitpunkt suchen wir an unserem <b>Standort in München</b> einen <b>Werkstudenten Contract (all genders)</b> für die Mediaplus, der größten inhabergeführten Mediaagentur Europas unter dem Dach der Serviceplan Group.</p>
+      <br><br>
+    <h2>Was dich bei uns erwartet:</h2>
+    <ul>
+      <li>Du überträgst Vertragsinformationen aus Kundenverträgen und extrahierst diese in die Vertragsdatenbank.</li>
+      <li>Bewertung und Klassifizierung von Verträgen.</li>
+      <li>Enge Zusammenarbeit mit dem Team, um sicherzustellen, dasa alle Vertragsinformationen vollständig und korrekt sind.</li>
+      <li>Unterstützung bei der Erstellung von Vertragsdokumenten und -vorlagen.</li>
+      <li>Du unterstützt bei der Durchführung von Vertragsprüfungen und Compliance-Checks.</li>
+    </ul>
+    <br> <br>
+    <h2>Was wir von dir erwarten:</h2>
+  <ul>
+    <li>Erstellung und Überprüfung von rechtlich relevanten Dokumenten.</li>
+    <li>
+      Entwurf und Verhandlung verschiedener Arten von Verträgen in englischer und
+      deutscher Sprache (z. B. Sponsoring-, Lieferanten- und sportspezifische
+      Lizenzverträge) mit Schwerpunkt auf Sponsoring, geistigem Eigentum,
+      gewerblichem Schutzrechten, Medien- und Sportrechten.
+    </li>
+  </ul>
+  <br> <br>
+  <h2>Qualifikationen</h2>
+  <ul>
+    <li>Du befindest dich im Studium der Rechtswissenschaft oder studierst Wirtschaftsrecht.</li>
+    <li>Du verfügst über fundierte Kenntnisse des deutschen Rechts.</li>
+    <li>Du hast Spaß an der Arbeit im agilen Projektmanagement.</li>
+    <li>
+      Du verfügst über eine strukturierte, pro-aktive und detaillierte Arbeitsweise und
+      besitzt ausgezeichnete Kenntnisse im Umgang mit MS-Office.
+    </li>
+    <li>
+      Du sprühst vor Teamgeist und überzeugst durch deine ausgeprägte
+      Kommunikationsfähigkeit, deinem Verantwortungsbewusstsein und deiner
+      praktischen und lösungsorientierten Einstellung.
+    </li>
+    <li>Du verfügst über fundiertes Urteilsvermögen und Verhandlungsgeschick.</li>
+    <li>Fließende Deutsch- und Englischkenntnisse in Wort und Schrift runden dein Profil ab.</li>
+  </ul>
+<br> <br>
+  <h2>Zusätzliche Informationen</h2>
+  <p>
+    Als Arbeitgeber schätzen wir Diversität und unterstützen Menschen dabei, ihre
+    Potenziale und Stärken zu entfalten, ihre Ideen zu verwirklichen und Chancen
+    wahrzunehmen. Die Stellenanzeige richtet sich an alle Menschen gleichermaßen,
+    unabhängig von Weltanschauung, Alter, Religion, Behinderung, Geschlecht, sexueller
+    Identität oder ethnischer Herkunft.
+  </p>
     `,
   }
 ];
